da08cc914a239d0e81dac8a06646ad197f885ae2
jdsk88 <jdsk88@gmail.com>
2022-02-26 21:26:08 +0100

    add missed store state update

--- frontend/src/services/events/index.js
+++ frontend/src/services/events/index.js
@@ -5,15 +5,19 @@ import {
   DELETE_ONE_ITEM,
   DELETE_ALL_ITEMS,
   SET_ITEMS,
+  GET_ITEMS,
 } from "store/actions";
 
 const EventsServices = {
   getAll: async (dispatch) => {
     const res = await api.get(`/events`);
-    return dispatch({
-      type: SET_ITEMS,
-      payload: res.data,
-    });
+    return (
+      dispatch({
+        type: SET_ITEMS,
+        payload: res.data,
+      }),
+      dispatch({ type: GET_ITEMS })
+    );
   },
 };
 export default EventsServices;
