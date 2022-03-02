c2459f8d21d70d5b52082cab4eadece2f85a8426
jdsk88 <jdsk88@gmail.com>
2022-03-03 00:31:41 +0100

    endpoint tests

--- api/src/tests/events.test.js
+++ api/src/tests/events.test.js
@@ -1,6 +1,10 @@
+import mongoose from "mongoose";
 import request from "supertest";
+import { env } from "../../../setupEnv";
 import { app } from "../index";
-// console.log(process.env.HOST);
+import { items } from "./initialState";
+
+let _id;
 const expectedKeys = [
   "__v",
   "_id",
@@ -10,16 +14,26 @@ const expectedKeys = [
   "lastName",
 ];
 
+let server;
+
+beforeAll(async () => {
+  await mongoose.connect(env.MONGO_DB);
+  server = app.listen(4000, () => {
+    global.agent = request.agent(server);
+  });
+});
+
+afterAll(async () => {
+  await server.close();
+  await mongoose.disconnect();
+});
 describe("EVENT POST ENDPOINT TEST", () => {
-  it("should create a new event item", async () => {
-    const res = await request(app).post("/api/events/create").send({
-      firstName: "Maciek",
-      lastName: "Jakobszy",
-      email: "jdsk88@gmail.com",
-      eventDate: 1646733780000,
+  items.forEach((item) => {
+    it("should create a new event item", async () => {
+      const res = await request(app).post("/api/events/create").send(item);
+      expect(res.statusCode).toEqual(200);
+      expect(Object.keys(res.body).sort()).toEqual(expectedKeys.sort());
     });
-    expect(res.statusCode).toEqual(200);
-    expect(Object.keys(res.body).sort()).toEqual(expectedKeys.sort());
   });
 });
 
@@ -31,10 +45,33 @@ describe("EVENT GET ENDPOINT TEST", () => {
   });
 });
 
-describe("EVENT DELETE ENDPOINT TEST", (id) => {
-  it("should get event list or empty array", async () => {
-    const res = await request(app).delete(`/api/events/${id}}`);
+describe("EVENT GET ITEM _id TEST", () => {
+  it("should get _id of item from 1st test result", async () => {
+    const res = await request(app).get("/api/events");
+    _id = res.body[0]._id;
     expect(res.statusCode).toEqual(200);
-    expect(res.body);
+    expect(res.body[0]._id);
+  });
+});
+
+// describe("EVENT PUT ITEM _id TEST", () => {
+//   it("should update item of 1st test result", async () => {
+//     const res = await request(app).put(`/api/events/${_id}`).send(itemUpdate);
+//     expect(res.statusCode).toEqual(200);
+//     expect(Object.keys(res.body).sort()).toEqual(expectedKeys.sort());
+//   });
+// });
+
+describe("EVENT DELETE ONE ITEM ENDPOINT TEST", () => {
+  it("should delete 1st test result", async () => {
+    const res = await request(app).delete(`/api/events/delete/${_id}`);
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+describe("EVENT DELETE ALL ITEMS ENDPOINT TEST", () => {
+  it("should delete all items", async () => {
+    const res = await request(app).delete(`/api/events/delete`);
+    expect(res.statusCode).toEqual(200);
   });
 });
